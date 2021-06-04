4ba238c1be0344fe3d966651056481805ee4b918
UnnervingChicken <63884439+UnnervingChicken@users.noreply.github.com>
2021-06-04 16:07:24 -0700

    Update StartForm.java

--- StartForm.java
+++ StartForm.java
@@ -6,4 +6,8 @@ public class StartForm {
     private JPanel panel1;
     private JButton singlePlayerButton;
     private JButton multiPlayerButton;
+    
+    public JPanel getPanel1() {
+        return Panel1;
+    }
 }
